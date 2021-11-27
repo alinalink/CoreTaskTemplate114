5c1f04a27f99b42d99394b80781b2ba2fd9fde7b
alina <alinenoc01@mail.ru>
2021-11-28 03:37:08 +0400

    Commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,22 +4,6 @@ import java.sql.*;
 
 
 public class Util {
-    //настройка соеденения с БД
-//    private static final String dbUrl = "jdbc:mysql://localhost:3306/users?useSSL=false";
-//    private static final String dbUsername = "root";
-//    private static final String dbPassword = "1234098765";
-//
-//    public static Connection getConnection() {
-//        Connection connection = null;
-//        try {
-//            Class.forName("com.mysql.jdbc.Driver");
-//            connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-//        } catch (ClassNotFoundException e) {
-//            System.out.println("Could not load JDBC driver: " + e.getMessage());
-//        } catch (SQLException e) {
-//            System.out.println("Could not connect to DB: " + e.getMessage());
-//        }
-//        return connection;
 
     private static final String URL = "jdbc:mysql://localhost:3306/users?autoReconnect=true&useSSL=false";
     private static final String USERNAME = "root";
